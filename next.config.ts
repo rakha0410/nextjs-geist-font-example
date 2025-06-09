2d85c9af41daf985f8050a0b4c3fcb664c81a133
rakha0410 <andryansyahputra979@gmail.com>
2025-06-10 05:22:48 +0700

    Add next.config.ts 1749507767773

---
+++ next.config.ts
@@ -0,0 +1,20 @@
+import type { NextConfig } from 'next'
+
+const nextConfig: NextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'images.pexels.com',
+        pathname: '/photos/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'www.lempardalam.site',
+        pathname: '/desktop/img/**',
+      },
+    ],
+  },
+}
+
+export default nextConfig
